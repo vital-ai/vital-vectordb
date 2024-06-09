fbbb5bfd92dd94d940680f92357055472738290f
Marc Hadfield <marc@vital.ai>
2024-06-09 14:06:07 -0400

    sync

---
+++ app.py
@@ -0,0 +1,8 @@
+
+# vector db using hnswlib for implementation
+# meant to be a lightweight solution to keep
+# n indexes with active ones in memory
+
+# include rest api for getting vector from embedding model
+# and for calculating similarity scores
+# to be used when such info needs to be utilized in a non python process such as within a db stored procedure
